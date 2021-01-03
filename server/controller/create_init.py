ebb23851fda48a3a06161ab0b35cdb111632b393
Hatano <b0652754723@yahoo.co.jp>
2021-01-03 21:40:37 +0900

    初期化APIの更新

--- server/controller/create_init.py
+++ server/controller/create_init.py
@@ -1,1 +1,10 @@
 import csv_management
+
+def create_bingo(bingo_code, bingo_len, bingo_size):
+    val = {
+        'endpoint': 'init',
+        'code': bingo_code,
+        'bingo_len': bingo_len,
+        'bingo_size': bingo_size
+    }
+    return val
--- server/endpoint/endpoint_main.py
+++ server/endpoint/endpoint_main.py
@@ -2,11 +2,11 @@ from bottle import route, get, post, request
 import create_init
 
 
-@route('/init', method='GET')
+@get('/init')
 def init():
-    bingo_code = request.query.get('code')
-    val = {
-        'endpoint': 'init',
-        'code': bingo_code
-    }
+    body = request.json
+    bingo_code = body['bingo_code']
+    bingo_len = body['bingo_len']
+    bingo_size = body['bingo_size']
+    val = create_init.create_bingo(bingo_code, bingo_len, bingo_size)
     return val
